049dd9bf0b268de60ee98eda66e552a48f74d0e3
Natorion <natorionj@gmail.com>
2026-03-27 23:37:35 +0100

    added in you can pass into different tags so dj_agent can import it and edit these tags which would be a usable tool for the DJ agent to call

--- DJ_project/jamendo_client.py
+++ DJ_project/jamendo_client.py
@@ -3,14 +3,34 @@ import requests
 clientId = "f5b09fc6"
 baseUrl = "https://api.jamendo.com/v3.0/tracks/"
 
-params = {
-    "client_id": clientId,
-    "format": "json",
-    "limit": 5,
-    "tags": "electronic"
-}
+def getTracksByTag(tag, limit=10):
+    params = {
+        "client_id": clientId,
+        "format": "json",
+        "limit": limit,
+        "tags": tag,
+        "include": "musicinfo"
+    }
 
-response = requests.get(baseUrl, params=params)
-data = response.json()
+    response = requests.get(baseUrl, params=params)
+    response.raise_for_status()
+    data = response.json()
 
-print(data)
+    cleanedTracks = []
+
+    for track in data.get("results", []):
+        cleanedTracks.append({
+            "id": track.get("id"),
+            "name": track.get("name"),
+            "artist": track.get("artist_name"),
+            "audio": track.get("audio"),
+            "duration": track.get("duration"),
+            "musicinfo": track.get("musicinfo")
+        })
+
+    return cleanedTracks
+
+tracks = getTracksByTag("electronic", 5)
+
+for track in tracks:
+    print(track)
